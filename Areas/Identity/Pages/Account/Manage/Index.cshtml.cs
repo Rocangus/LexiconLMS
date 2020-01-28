038a976462046e529f2121096030fe1017d2f700
Rocangus <hunter_sniper112@hotmail.com>
2020-01-28 16:34:25 +0100

    Add possibility to edit user roles

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using LexiconLMS.Core.Models;
+using LexiconLMS.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace LexiconLMS.Areas.Identity.Pages.Account.Manage
 {
     public partial class IndexModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<SystemUser> _userManager;
+        private readonly SignInManager<SystemUser> _signInManager;
+        private readonly ApplicationDbContext _context;
 
         public IndexModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            UserManager<SystemUser> userManager,
+            SignInManager<SystemUser> signInManager,
+            ApplicationDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
 
-        public string Username { get; set; }
+        public string Name { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -32,21 +39,45 @@ namespace LexiconLMS.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            [Required]
+            public string Role { get; set; }
+            [EmailAddress]
+            public string Email { get; set; }
+
+            public List<SelectListItem> RoleItemList { get; set; }
         }
 
-        private async Task LoadAsync(IdentityUser user)
+        private async Task LoadAsync(SystemUser user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var name = user.Name; ;
+            var email = await _userManager.GetPhoneNumberAsync(user);
 
-            Username = userName;
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+
+            var roles = await _context.Roles.ToListAsync();
+            var currentRoleId = await _context.UserRoles.Where(r => r.UserId.Equals(user.Id)).Select(r => r.RoleId).FirstOrDefaultAsync();
+
+            foreach (var role in roles)
+            {
+
+                if (currentRoleId == null)
+                {
+                    throw new Exception("Failed to find the current user's role.");
+                }
+                var selectItem = new SelectListItem
+                {
+                    Text = role.Name,
+                    Selected = currentRoleId.Equals(role.Id)
+                };
+                selectListItems.Add(selectItem);
+            }
+
+            Name = name;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                Email = email,
+                RoleItemList = selectListItems
             };
         }
 
@@ -58,7 +89,14 @@ namespace LexiconLMS.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            await LoadAsync(user);
+            var systemUser = user;
+
+            if (systemUser == null)
+            {
+                throw new InvalidCastException("The IdentityUser could not be cast to a SystemUser.");
+            }
+
+            await LoadAsync(systemUser);
             return Page();
         }
 
@@ -76,14 +114,24 @@ namespace LexiconLMS.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
+            var selectedRole = Input.RoleItemList.Where(r => r.Selected).Select(r => r.Text).FirstOrDefault();
+            var currentRoleId = await _context.UserRoles.Where(r => r.UserId == user.Id).Select(r => r.UserId).FirstOrDefaultAsync();
+            var currentRole = await _context.Roles.Where(r => r.Id == currentRoleId).FirstOrDefaultAsync();
+
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            if (!currentRole.Name.Equals(selectedRole))
             {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
+                var roleAddResult = await _userManager.AddToRoleAsync(user, selectedRole);
+                if (!roleAddResult.Succeeded)
                 {
                     var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                    throw new InvalidOperationException($"Unexpected error occurred adding user with user ID '{userId}' to role '{currentRole.Name}'.");
+                }
+                var roleRemoveResult = await _userManager.RemoveFromRoleAsync(user, currentRole.Name);
+                if (!roleRemoveResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred removing user with user ID '{userId}' from role '{currentRole.Name}'.");
                 }
             }
 
