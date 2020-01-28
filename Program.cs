d4dcc41ca864161f0ef526d813f9c5ec674b80f5
Rocangus <hunter_sniper112@hotmail.com>
2020-01-28 11:41:15 +0100

    Seed aministrator user

--- Program.cs
+++ Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LexiconLMS.Core.Models;
+using LexiconLMS.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +17,26 @@ namespace LexiconLMS
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services, services.GetRequiredService<IConfiguration>(),
+                           services.GetRequiredService<UserManager<SystemUser>>(),
+                           services.GetRequiredService<RoleManager<IdentityRole>>()).Wait();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e.Message, "Seed failed");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
