c83205f134cfe48ab97bc7c99602f84cf67ab194
OlSarLang <o.sarholm@hotmail.com>
2020-02-05 10:35:37 +0100

    Added more authentication, removed role selection for student, auto redirect to login for unauthorized users

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LexiconLMS.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LexiconLMS.Controllers
 {
@@ -18,6 +19,7 @@ namespace LexiconLMS.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
