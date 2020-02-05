133b11315d9aff130db2a3f3d22dd26831f30b3c
sirajbd0176 <sirajbd0176@gmail.com>
2020-02-05 13:38:59 +0100

    Unignore important document files

---
+++ Core/Services/IDocumentService.cs
@@ -0,0 +1,20 @@
+﻿using LexiconLMS.Core.Models.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Core.Services
+{
+    interface IDocumentService
+    {
+        Task<IEnumerable<Document>> GetCourseDocumentsAsync(int id);
+        Task<IEnumerable<Document>> GetModuleDocumentsAsync(int id);
+        Task<IEnumerable<Document>> GetActivityDocumentsAsync(int id);
+        Task<IEnumerable<Document>> GetAssignmentDocumentsAsync(int id);
+        Task<Document> GetUserAssignmenDocumentAsync(string id);
+
+
+
+    }
+}
