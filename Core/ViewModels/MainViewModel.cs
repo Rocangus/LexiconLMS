f55b6ff0e98b364414e1133059b6c2c9f65287bc
OlSarLang <o.sarholm@hotmail.com>
2020-02-07 11:10:55 +0100

    Pushed missing model and view

---
+++ Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LexiconLMS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Core.ViewModels
+{
+    public class MainViewModel
+    {
+        public SystemUserViewModel SystemUser { get; set; }
+        public Course Course { get; set; }
+    }
+}
